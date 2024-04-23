fd313137bbb8f02b33bbd5ac6b4839763174ccb9
Qudus Lawal <lawalqudus980@gmail.com>
2024-04-23 21:42:04 +0100

    chores: Made brandLogo a component

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,8 +1,8 @@
-import logo from '../assets/logo.svg';
 import shoppingCart from '../assets/shoppingcart.svg';
 import hamburger from '../assets/hamburger.svg'
 import { useState } from 'react';
 import NavList from './components/NavList';
+import BrandLogo from './components/BrandLogo';
 
 const Navbar = () => {
 
@@ -16,19 +16,17 @@ const Navbar = () => {
         
         <nav className='flex items-center justify-center h-20 bg-transparent shadow'>
             <div className='container mx-auto px-4 flex justify-between items-center w-full h-full'>
-                <a href="" className='bg-[#0c513f] rounded-full h-12 w-12 pl-3 pt-3 pb-2 pr-2'>
-                    <img src={logo} alt=""/>
-                </a>
+                <BrandLogo/>
             
                 <div className='flex gap-3'>
                     <a href="" className='bg-white rounded-full p-3'>
                         <img src={shoppingCart} alt="" />
                     </a>
-                    <div>
+                    <div className='lg:hidden'>
                         <button className='pb-3 bg-[#0c513f] rounded-full p-3 ' onClick={toggleMenu}>
-                        <img src={hamburger} alt="" />
-                    </button>
-                    {isOpen && <NavList onClose={() => setIsOpen(false)}/>}
+                            <img src={hamburger} alt="" />
+                        </button>
+                        {isOpen && <NavList onClose={() => setIsOpen(false)}/>}
                     </div>
                     
                 </div>
