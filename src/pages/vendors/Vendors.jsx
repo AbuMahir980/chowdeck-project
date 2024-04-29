f79b393974289a5f0847086babf930e3019577c7
Qudus Lawal <lawalqudus980@gmail.com>
2024-04-29 16:12:37 +0100

    chores: add new to vendors

--- src/pages/vendors/Vendors.jsx
+++ src/pages/vendors/Vendors.jsx
@@ -10,11 +10,14 @@ const Vendors = () => {
         <div>
             <Navbar />
             <div 
-            className="relative bg-cover bg-center h-screen flex flex-col items-center justify-center gap-4 bg-hero">
-                <h1 className='font-bold text-6xl text-center mt-40 tracking-tighter align-bottom md:container md:text-left md:text-8xl md:text-balance md:ml-5 lg:container lg:px-auto lg:text-left lg:text-balance lg:text-9xl lg:mr-6 lg:leading-tight lg:tracking-tighter mb-5'>Grow your Food Business</h1>
+            className="relative min-h-screen flex flex-col items-center justify-center gap-4 bg-hero bg-no-repeat md:bg-cover ">
+                <h1 className='font-bold text-6xl text-center mt-40 tracking-tighter align-bottom md:container md:text-left md:text-8xl md:text-balance md:ml-5 lg:container lg:px-auto lg:text-left lg:text-balance lg:text-9xl lg:ml-5 lg:leading-tight lg:tracking-tighter mb-5'>Grow your Food Business</h1>
 
-                <Buttons img={playStore} bgColor="bg-black" text="Download on Google Play" width="w-72" height="h-16" textWeight="font-medium" textSize="text-base"/>
-                <Buttons img={appStore} bgColor="bg-black" text="Download on App Store" width="w-72" height="h-16" textWeight="font-medium" textSize="text-base"/>
+                <div className='md:flex md:flex-row gap-3 md:justify-start md:self-start md:container md:ml-5 lg:container lg:mx-auto'>
+                    <Buttons img={playStore} bgColor="bg-black" text="Download on Google Play" width="w-72" height="h-16" textWeight="font-medium" textSize="text-base"/>
+                    <Buttons img={appStore} bgColor="bg-black" text="Download on App Store" width="w-72" height="h-16" textWeight="font-medium" textSize="text-base"/>
+                </div>
+                
             </div>
         </div>
     ); 
