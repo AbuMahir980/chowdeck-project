6a130c7af7f2784ffb6e882bfce6b167ad2e760a
Taofeek Toriola <toriolataofeekolamilekan@gmail.com>
2024-04-29 18:03:52 +0100

    update

--- src/pages/customersPage/Customer.jsx
+++ src/pages/customersPage/Customer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from './shared/Button'
-import playStoreImg from "../../assets/playStore.svg"
+import playStoreImg from "../../assets/customersImg/playStore.svg"
 import appStore from "../../assets/customersImg/appStore.svg"
 import Appads from './Appads'
 const Customer = () => {
@@ -8,8 +8,8 @@ const Customer = () => {
     <div className="">
     <div className=' bg-bgImage w-[100%] h-[100vh] font-plusJakarta'>
       <div className="pt-12">
-        <div className="text-[112px] font-[700] text-center flex justify-center items-center">
-        <h1 className="">Have you eaten?</h1>
+        <div className=" font-[700] text-center flex justify-center items-center">
+        <h1 className="lg:text-[112px]">Have you eaten?</h1>
         </div>
         <div className="flex gap-4 justify-center items-center">
         <div className="bg-[#0c513f] text-white rounded-[7px]"><Button image={playStoreImg} text="Download on Google Play"/></div>
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,61 +1,14 @@
-<<<<<<< .merge_file_qw8rAQ
-// import logo from '../assets/logo.svg'
-// import dropdown from '../assets/chevroletdown.svg'
-// import shoppingcart from '../assets/shoppingcart.svg'
-// import { useState } from 'react'
-=======
-import shoppingCart from '../assets/shoppingcart.svg';
-import hamburger from '../assets/hamburger.svg'
-import { useState } from 'react';
-import NavList from './components/NavList';
-import BrandLogo from './components/BrandLogo';
-import LgMenuList from './components/LgMenuList';
-import Client from './components/Clients';
->>>>>>> .merge_file_IMzswh
+import logo from '../assets/logo.svg'
+import dropdown from '../assets/chevroletdown.svg'
+import shoppingcart from '../assets/shoppingcart.svg'
+import hamburger from "../assets/hamburger.svg"
+import { useState } from 'react'
+import BrandLogo from './components/BrandLogo'
+import LgMenuList from './components/LgMenuList'
+import NavList from './components/NavList'
 
-// const Navbar = () => {
+const Navbar = () => {
 
-<<<<<<< .merge_file_qw8rAQ
-//     // const [dropDown, setDropDown] = useState(false)
-    
-//     // const handleDropDown = () => {
-//     //     setDropDown(!dropDown)
-//     // }
-//     const [isHovered, setIsHovered] = useState(false);
-//     const handleMouseOver = () => {
-//         setIsHovered(true);
-//     };
-
-//     const handleMouseOut = () => {
-//         setIsHovered(false);
-//     };
-
-//     return(
-//             <nav className='flex flex-row justify-between items-center'>
-//                 <div className=''>
-//                     <a className='flex'>
-//                         <img src={logo} alt="Logo" /><span>chowdeck</span>
-//                     </a>
-//                 </div>
-//                 <div>
-//                     <a href="">Relay</a>
-//                     <a href="">Company</a>
-//                     <a href="">FAQs</a>
-//                     <a href="">Blog</a>
-//                     <a href="">Contact</a>
-//                 </div>
-//                 <div>
-//                     <button onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-//                         <span>Customers</span><img src={dropdown} alt="" />
-//                     </button>
-//                     <button>
-//                         <img src={shoppingcart} alt="" />
-//                     </button>
-//                 </div>
-//             </nav>
-//     )
-// }
-=======
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleMenu = () => {
@@ -73,7 +26,7 @@ import Client from './components/Clients';
                 <div className='flex gap-3'>
                     {/* <Client /> */}
                     <a href="" className='bg-white rounded-full p-3'>
-                        <img src={shoppingCart} alt="" />
+                        <img src={shoppingcart} alt="" />
                     </a>
                     <div className='lg:hidden'>
                         <button className='pb-3 bg-[#0c513f] rounded-full p-3 ' onClick={toggleMenu}>
@@ -89,6 +42,5 @@ import Client from './components/Clients';
         
     )
 }
->>>>>>> .merge_file_IMzswh
 
-// export default Navbar
+export default Navbar
