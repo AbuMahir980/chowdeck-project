bbb3f703d5d8c81ac1890650b351ee3528e06a63
Qudus Lawal <lawalqudus980@gmail.com>
2024-04-30 20:12:37 +0100

    chores: added update

--- src/pages/vendors/Vendors.jsx
+++ src/pages/vendors/Vendors.jsx
@@ -1,4 +1,4 @@
-import hero from '../../assets/vendors/hero.svg';
+// import hero from '../../assets/vendors/hero.svg';
 import Buttons from '../../components/vendorComponents/Buttons';
 import Navbar from '../../shared/Navbar';
 import playStore from '../../assets/vendors/googlePlay.svg';
@@ -7,17 +7,37 @@ import appStore from '../../assets/vendors/appStore.svg'
 
 const Vendors = () => {
     return (
-        <div>
+        <div className='w-screen'>
             <Navbar />
             <div 
-            className="relative min-h-screen flex flex-col items-center justify-center gap-4 bg-hero bg-no-repeat md:bg-cover ">
+            className="relative min-h-screen w-screen md:flex md:min-w-screen flex flex-col items-center justify-center gap-4 bg-hero bg-cover bg-no-repeat">
+                <div className='mb-10'></div>
                 <h1 className='font-bold text-6xl text-center mt-40 tracking-tighter align-bottom md:container md:text-left md:text-8xl md:text-balance md:ml-5 lg:container lg:px-auto lg:text-left lg:text-balance lg:text-9xl lg:ml-5 lg:leading-tight lg:tracking-tighter mb-5'>Grow your Food Business</h1>
 
-                <div className='md:flex md:flex-row gap-3 md:justify-start md:self-start md:container md:ml-5 lg:container lg:mx-auto'>
+                <div className='flex flex-col justify-center md:flex md:flex-row gap-4 md:justify-start md:self-start md:container md:ml-5 lg:container lg:mx-auto lg:px-4 xl:container xl:mx-auto xl:px-4'>
                     <Buttons img={playStore} bgColor="bg-black" text="Download on Google Play" width="w-72" height="h-16" textWeight="font-medium" textSize="text-base"/>
                     <Buttons img={appStore} bgColor="bg-black" text="Download on App Store" width="w-72" height="h-16" textWeight="font-medium" textSize="text-base"/>
                 </div>
-                
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <div className='mb-10'></div>
+                <h2 className='font-bold text-[32px] leading-[48px] text-center mb-5 mt-10 md:text-[48px] md:leading-[48px] md:text-center lg:text-[64px] lg:leading-[80px]'>Get Started, download the app.</h2>
+                <div className='flex flex-col justify-center md:flex md:flex-row gap-4 md:justify-center'>
+                    <Buttons img={playStore} bgColor="bg-[#0c513f]" text="Download on Google Play" width="w-72" height="h-16" textWeight="font-medium" textSize="text-base"/>
+                    <Buttons img={appStore} bgColor="bg-[#0c513f]" text="Download on App Store" width="w-72" height="h-16" textWeight="font-medium" textSize="text-base"/>
+                </div>
             </div>
         </div>
     ); 
