1defffba74d19c5aaab3cf6ccab560b65bc24e41
Al-Ameen Abdul-Razaq <alameenabdulrazaq2013@gmail.com>
2024-05-03 15:42:18 +0100

    fridayChowdeck1

--- src/components/vendorComponents/ChowdeckPort2.jsx
+++ src/components/vendorComponents/ChowdeckPort2.jsx
@@ -5,64 +5,70 @@ import chowOwner from "../../assets/chowdeck/1698762681.jpg";
 const ChowdeckPort2 = () => {
   return (
     <div>
-      <div className="px-16 pb-28 ">
-        <div className=" border-solid-9 outline border-black rounded-lg bg-white ">
+      <div className="px-16  ">
+        <div className=" border-4 border-black rounded-t-lg bg-white ">
           <span>
-            <div className="flex border-black outline-none border-solid">
-              <p className="bg-[#0C513F] p-6 box-border text-2xl text-white flex font-bold w-[500px]">
-                <span src={chowFounder}></span>Founder's Story
-              </p>
+            <div className="flex justify-around  border-black outline-none border-solid">
+              <div className="border-black border-4">
+                <p className="bg-[#0C513F] p-6 box-border rounded-lg-lg text-2xl text-white flex font-bold w-[500px]">
+                  <span src={chowFounder}></span>Founder's Story
+                </p>
+
+                <img
+                  src={chowOwner}
+                  className="mt-14 mx-14 h-[500px] w-[400px] "
+                />
+              </div>
+
+              {/* <span className="mt-32 h-[1900px] w-[2400px]"></span> */}
+
+              <span className="">
+                <p>
+                  I tested positive for COVID on the 31st of December, 2020. The
+                  result came in very late at night and I couldn’t travel home.
+                  I spent the entire January 1 looking for food vendors to
+                  deliver food to me but the available food delivery providers
+                  didn’t deliver during public holidays. I eventually found one
+                  after so many hours and ended up paying 4x the regular amount.
+                </p>
+                <p>
+                  During my 14- day isolation, I realised no one was really
+                  looking into the logistics problem with the care and attention
+                  I would have wished for. I started asking questions; most
+                  answers ended with “dispatch riders are not reliable”, then I
+                  made a lot of research and discovered delivery companies in
+                  countries like India made millions of deliveries daily. No one
+                  comes close locally and it didn’t make sense that at such a
+                  small scale, we Africans couldn’t figure it out.
+                </p>
+                <p>
+                  We initially approached the problem from a technical
+                  standpoint then realised the problem was more operations than
+                  tech and we needed to deconstruct operations before trying
+                  again. We went back to first principles to figure this out. We
+                  pulled funds from our pockets, got three bikes and riders -
+                  gave two of them to Korede Spaghetti and one of them to
+                  NiFries. We held meetings weekly with our riders and that gave
+                  us so much insight we needed.
+                </p>
+                <p>
+                  With a better understanding of what we believed the problem
+                  was, we knew our solution had to be efficient and easy to use.
+                  This was hard to pull as we struggled with iterations before
+                  settling with one. It was a lot harder building the product as
+                  we had to ensure equal dedication to all channels (riders,
+                  restaurants and customers).
+                </p>
+                <p>
+                  We pulled our tech force and built the first version of the
+                  app in 3 weeks and launched public beta in October. Since
+                  then, it’s been an interesting journey learning and building
+                  for our customers.
+                </p>
+              </span>
             </div>
 
-            <div className="border-black outline-4 border-solid-5"></div>
-            {/* <span className="text-right pl-40 w-[746px] ml-80">
-              <p>
-                I tested positive for COVID on the 31st of December, 2020. The
-                result came in very late at night and I couldn’t travel home. I
-                spent the entire January 1 looking for food vendors to deliver
-                food to me but the available food delivery providers didn’t
-                deliver during public holidays. I eventually found one after so
-                many hours and ended up paying 4x the regular amount.
-              </p>
-              <p>
-                During my 14- day isolation, I realised no one was really
-                looking into the logistics problem with the care and attention I
-                would have wished for. I started asking questions; most answers
-                ended with “dispatch riders are not reliable”, then I made a lot
-                of research and discovered delivery companies in countries like
-                India made millions of deliveries daily. No one comes close
-                locally and it didn’t make sense that at such a small scale, we
-                Africans couldn’t figure it out.
-              </p>
-              <p>
-                We initially approached the problem from a technical standpoint
-                then realised the problem was more operations than tech and we
-                needed to deconstruct operations before trying again. We went
-                back to first principles to figure this out. We pulled funds
-                from our pockets, got three bikes and riders - gave two of them
-                to Korede Spaghetti and one of them to NiFries. We held meetings
-                weekly with our riders and that gave us so much insight we
-                needed.
-              </p>
-              <p>
-                With a better understanding of what we believed the problem was,
-                we knew our solution had to be efficient and easy to use. This
-                was hard to pull as we struggled with iterations before settling
-                with one. It was a lot harder building the product as we had to
-                ensure equal dedication to all channels (riders, restaurants and
-                customers).
-              </p>
-              <p>
-                We pulled our tech force and built the first version of the app
-                in 3 weeks and launched public beta in October. Since then, it’s
-                been an interesting journey learning and building for our
-                customers.
-              </p>
-            </span> */}
-
-            <div className="mt-14 mx-14">
-              <img src={chowOwner} height="450px" width="380px" />
-            </div>
+            <div className="border-black outline flex border-solid-9 border-r-4"></div>
 
             <div>
               <h2 className="mx-32 mt-6 text-2xl font-normal">
--- src/components/vendorComponents/Company.jsx
+++ src/components/vendorComponents/Company.jsx
@@ -9,13 +9,13 @@ import Navbar from '../../shared/Navbar'
 const Company = () => {
   return (
     <div>
-      <div className="bg-[#FFEDB3] bg-cover min-h-[100vh] px-2">
+      <div className="bg-[#FFEDB3] bg-cover min-h-[50vh] px-2">
         <Navbar />
-        <div className="h-[8rem] "></div>
+        <div className="h-[4rem] "></div>
         <div className="mx-9 font-bold mt-10 ">
           <h1 className="mx-11 font-bold text-9xl">Delivery Happiness</h1>
         </div>
-        <div className="mt-72">
+        <div className="mt-56">
           <div className="flex mx-4">
             <img src={hoverItems} className="ml-16  md:w-[130px]" />
             <img src={hoverItems2} className="md:w-[700px] ml-96" />
@@ -24,13 +24,7 @@ const Company = () => {
       </div>
 
       <ChowdeckPort2 />
-      <div className="mx-auto border-solid-5  border-black flex rounded-md w-[90%] h-[10rem]">
-        <div className="w-[90%]  h-32">
-          <div className="w-[30%] border-solid-5 border-black p-7"></div>
-          <div className="w-[70%] border-solid-5 border-black p-7"></div>
-        </div>
-        <div className="w-[90%] border-solid-5 outline border-black flex rounded-md p-8 h-24"></div>
-      </div>
+      
       <ChowdeckMovingImages />
     </div>
   );
